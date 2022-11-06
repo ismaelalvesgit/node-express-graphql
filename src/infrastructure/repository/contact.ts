1af3472045e7563b7f128678d852044b12f0690c
ismael alves <cearaismael1997@gmail.com>
2022-11-06 00:35:50 -0300

    update reposioty

--- src/infrastructure/repository/contact.ts
+++ src/infrastructure/repository/contact.ts
@@ -1,31 +1,52 @@
 import {
+  IMessageBusAdapter,
+  IMessageBusAdapterConstructs,
   IMysqlAdapter,
 } from "@type/infrastructure";
 import { Contact, IContactRepository } from "@type/contact";
-import { Server } from "socket.io";
+import { Pagination } from "@type/interface";
+import R from "ramda";
+import pubsub from "@graphql/helpers/pubsub";
+import { SubscribeNameSpace } from "@type/system";
+import { env } from "@util/env";
 
 export type Context = {
   mysqlAdapter: IMysqlAdapter;
-  socket?: Server;
+  messageBusAdapter: IMessageBusAdapterConstructs;
 };
 
 export class ContactRepository implements IContactRepository {
   private mysqlAdapter: Context["mysqlAdapter"];
-  private socket?: Context["socket"];
+  private messageBusAdapter: IMessageBusAdapter;
 
-  constructor({ mysqlAdapter, socket }: Context) {
+  constructor({ mysqlAdapter, messageBusAdapter }: Context) {
     this.mysqlAdapter = mysqlAdapter;
     this.mysqlAdapter.tableName = "contact";
-    this.socket = socket;
+    this.messageBusAdapter = new messageBusAdapter();
   }
 
-  public async find(): Promise<Contact[]> {
-    return this.mysqlAdapter.db;
+  public async find(filter: Partial<Contact>, pagination: Pagination): Promise<Contact[]> {
+    const { page, size, sort, sortBy } = pagination;
+    return this.mysqlAdapter.db
+      .where(R.reject(R.isNil, {...filter}))
+      .offset(page || 0)
+      .limit(size || 100)
+      .orderBy(sortBy || "name", sort || "asc");
+  }
+
+  async asyncCreate(contact: Contact): Promise<void> {
+    await this.messageBusAdapter.publish(
+      env.amqp.exchanges.example.key,
+      env.amqp.exchanges.example.routing,
+      contact
+    );
   }
 
   public async create(contact: Contact): Promise<void> {
     await this.mysqlAdapter.db.insert(contact);
-    this.socket?.emit("/contact", contact);
+    pubsub.publish(SubscribeNameSpace.POST_CONTACT_CREATED, {
+      contactCreated: contact,
+    });
   }
 
   public async update(contact: Contact): Promise<void> {
--- src/infrastructure/repository/system.ts
+++ src/infrastructure/repository/system.ts
@@ -3,9 +3,8 @@ import {
   IRedisAdapter,
   IRedisAdapterConstructs,
 } from "@type/infrastructure";
-import { ISystemRepository, SystemMetric } from "@type/system";
+import { ISystemRepository } from "@type/system";
 import knex from "@util/knex";
-import { register } from "prom-client";
 
 export type Context = {
   config: Env;
@@ -27,27 +26,6 @@ export class SystemRepository implements ISystemRepository {
 
   healthcheck(): Promise<void> {
     return knex.raw("select 1+1 as result");
-  }
-
-  async metrics(): Promise<SystemMetric> {
-    const metrics = await register.metrics();
-    const contentType = register.contentType;
-    return {
-      metrics,
-      contentType
-    };
-  }
-
-  metricExcludeUrl(){
-    return [
-      "/",
-      "/favicon.ico",
-      "/system/metrics",
-      "/system/healthcheck",
-      "/v1/system/metrics",
-      "/v1/system/healthcheck",
-      "/v1/static/uploads/system/default.png"
-    ];
   }
 
   redis(): IRedisAdapter {
