e03d79f9239eddd2c516ee6d35f640d413f176ba
ismael alves <cearaismael1997@gmail.com>
2022-12-16 19:03:45 -0300

    update test

--- src/test/container.mock.ts
+++ src/test/container.mock.ts
@@ -1,6 +1,8 @@
+import { cepMock } from "@repository/__tests__/__mocks__/brasilRepository.mock";
+import { ContactService } from "@core/service/contact";
+import { SystemService } from "@core/service/system";
 import { Chance } from "chance";
 import { ContactRepository } from "@repository/contact";
-import { cepMock } from "@repository/__tests__/__mocks__/brasilRepository.mock";
 import { Contact } from "@type/contact";
 import { MysqlAdapter } from "@adapter/mysql";
 import { RedisAdapter } from "@adapter/redis";
@@ -18,6 +20,14 @@ export const serviceMock = {
     systemRepository: new SystemRepository({config: env.get(), redisAdapter: RedisAdapter}),
     contactRepository: new ContactRepository({mysqlAdapter,  messageBusAdapter: messageBusAdapterMock }),
     brasilRepository: {
+        getCep: jest.fn().mockResolvedValue({ ...cepMock }),
+    },
+}; 
+
+export const userCaseMock = {
+    systemService: new SystemService(serviceMock),
+    contactService: new ContactService(serviceMock),
+    brasilService: {
         getCep: jest.fn().mockResolvedValue({ ...cepMock }),
     },
 }; 
